79aa229f467f8fb588263d986ad6f3898fddf8ed
Alex Lee <525210804@qq.com>
2017-12-21 17:03:03 +0800

    Update main.py
    Added code in main

---
+++ main.py
@@ -0,0 +1,8 @@
+from module_utility.LearningMarketsToturial.obtain_video_list import get_video_link
+from module_utility.LearningMarketsToturial.video_download import download_video
+
+def main():
+    get_video_link()
+    download_video()
+
+main()
