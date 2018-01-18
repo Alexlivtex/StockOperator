3aceb34b32e4d9b6976af6d7732d951e9785fad4
Alex Lee <525210804@qq.com>
2018-01-18 10:50:18 +0800

    Update main.py
    Added code to fetch stock symbol

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from module_utility.LearningMarketsToturial.obtain_video_list import get_video_link
 from module_utility.LearningMarketsToturial.video_download import download_video
+from module_utility.StockDataObtain.get_stock_ticker import fetch_stock_symbol
 
 def main():
     get_video_link()
     download_video()
+    fetch_stock_symbol()
 
 main()
